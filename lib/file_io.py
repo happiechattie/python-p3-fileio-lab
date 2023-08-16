8ad0be7718ab3d61527b070b98c92b4898983a83
Christine Baik <christuaik@gmail.com>
2023-08-16 16:09:50 -0700

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as writer:
+        writer.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as writer:
+        writer.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as reader:
+        return reader.read()
